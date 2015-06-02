1fa2e0ca1996d2e40f52b09b8cf22cdec5463911
Po-Ying Chen <poying.me@gmail.com>
2015-06-02 13:26:36 +0800

    rewrite

--- index.js
+++ index.js
@@ -2,29 +2,33 @@
 
 var qs = require('qs');
 var url = require('url');
+var reg = /\/{2,}/;
 
 module.exports = function urlFormat(reqUrl) {
-  var reg;
-  var newUrl = url.parse(reqUrl);
-  var query = qs.parse(newUrl.query);
+  var parsedUrl = url.parse(reqUrl);
+  var query = qs.parse(parsedUrl.query);
 
-  reg = /\/{2,}/g;
-  newUrl.pathname = newUrl.pathname.replace(reg, '/');
-  newUrl.path = newUrl.path.replace(reg, '/');
-  Object.keys(query).forEach(function(data) {
-    if (query[data] === '') {
-      delete query[data];
-    }
-  });
-  newUrl.query = query;
-  newUrl.query = qs.stringify(query);
-  if (newUrl.query !== '') {
-    newUrl.search = '?' + newUrl.query;
-  }
-  else {
-    newUrl.search = '';
-  }
-  newUrl.path = newUrl.path + newUrl.query;
-  newUrl = url.format(newUrl);
-  return newUrl;
+  cleanObject(query);
+
+  parsedUrl.query = qs.stringify(query);
+
+  parsedUrl.search = parsedUrl.query
+    ? '?' + parsedUrl.query
+    : '';
+
+  parsedUrl.pathname = ''
+    + parsedUrl.path.replace(reg, '/')
+    + parsedUrl.search;
+
+  return url.format(parsedUrl);
 };
+
+function cleanObject(obj) {
+  Object
+    .keys(obj)
+    .forEach(function(key) {
+      if (!obj[key]) {
+        delete obj[key];
+      }
+    });
+}
